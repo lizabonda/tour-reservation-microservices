848d20fade4e73fd361778b6e337b86a5fddc1a1
Elizaveta Bondarenko <libondarenko10@gmail.com>
2026-04-20 16:49:06 +0200

    Configuration of discovery server

---
+++ discovery-server/src/main/java/cz/cvut/fel/nss/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package cz.cvut.fel.nss.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+
+}
